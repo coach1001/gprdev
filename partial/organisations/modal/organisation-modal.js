7e029236501a6f4a7c14b12c5d6a9ca532118c9d
Francois Weber <fweberfhr.org.za>
2016-03-10 13:01:04 +0200

    gprdev.sql Update

---
+++ partial/organisations/modal/organisation-modal.js
@@ -0,0 +1,112 @@
+angular.module('appCg').controller('OrganisationModalCtrl', function(
+    organisation,
+    referees,
+    auditors,
+    provinces,
+    orgTypes,
+    orgStatuses,
+    suburbs,
+    places,
+    operation,
+    gprRestApi,
+    ngToast,
+    $confirm,
+    $uibModalInstance) {
+
+    var vm = this;
+
+    if (operation === 'Create') { vm.organisation = {}; } else if (operation === 'Update') { vm.organisation = organisation.selectedRow; }
+
+    //console.log(provinces);
+    vm.operation = angular.extend(operation);
+		//vm.referees = angular.extend(referees.rows);
+    //vm.auditors = angular.extend(auditors.rows);
+    vm.provinces = angular.extend(provinces.rows);
+    //vm.orgTypes = angular.extend(orgTypes.rows);
+    //vm.orgStatuses = angular.extend(orgStatuses.rows);
+    //vm.suburbs = angular.extend(suburbs.rows);
+    //vm.places = angular.extend(places.rows);
+    
+    console.log(vm.provinces);
+
+    vm.organisationFields = [{
+            key: 'name',
+            type: 'input',
+            templateOptions: {
+                type: 'text',
+                label: 'Organisation Name',
+                placeholder: 'Organisation Name',
+                required: true
+            }
+        }, {
+            key: 'province',
+            type: 'ui-select-single',
+            templateOptions: {
+                optionsAttr: 'bs-options',
+                ngOptions: 'option[to.valueProp] as option in to.options | filter: $select.search',
+                label: 'Province',
+                valueProp: 'id',
+                labelProp: 'name',
+                required: false,
+                options: vm.provinces
+            }
+        }
+        /*
+                , {
+                        fieldGroup: [{
+                            className: 'col-xs-4 nopadding',
+                            key: 'code',
+                            type: 'input',
+                            templateOptions: {
+                                type: 'text',
+                                label: 'Code',
+                                placeholder: 'Programme Code',
+                                required: true
+                            }
+                        }, {
+                            className: 'col-xs-8 nopadding',
+                            key: 'name',
+                            type: 'input',
+                            templateOptions: {
+                                type: 'text',
+                                label: 'Name',
+                                placeholder: 'Programme Name',
+                                required: true
+                            }
+                        }]
+                    }, {
+                        key: 'description',
+                        type: 'textarea',
+                        className: 'nopadding',
+                        templateOptions: {
+                            label: 'Description',
+                            placeholder: 'Programme Description',
+                            rows: 3,
+                            required: false
+                        }
+                    }*/
+    ];
+
+    vm.updateCreateRow = function() {
+        var body = angular.copy(vm.organisation);
+
+        gprRestApi.updateCreateRow('organisations', body, vm.operation).then(function success(response) {
+            ngToast.create({ content: vm.operation + ' Record Successfull', timeout: 4000 });
+            if (vm.operation === 'Create') { vm.organisation.id = response.data.id; }
+
+            vm.operation = 'Update';
+        }, function error(response) {
+            ngToast.warning({ content: vm.operation + ' Record Failed', timeout: 4000 });
+        });
+    };
+
+    vm.deleteRow = function() {
+        gprRestApi.deleteRow('organisations', vm.organisation.id).then(function success(response) {
+            ngToast.warning({ content: 'Record Deleted Successfully', timeout: 4000 });
+            $uibModalInstance.dismiss('Record Deleted');
+        }, function error(response) {
+            ngToast.warning({ content: 'Record Delete Failed', timeout: 4000 });
+        });
+
+    };
+});
--- partial/organisations/organisations.js
+++ partial/organisations/organisations.js
@@ -1,11 +1,11 @@
-angular.module('appCg').controller('OrganisationsCtrl', function(grid_organisations,gprRestApi, $uibModal, $filter, $state, uiGridConstants) {
+angular.module('appCg').controller('OrganisationsCtrl', function(grid_organisations, gprRestApi, $uibModal, $filter, $state, uiGridConstants) {
     var vm = this;
     vm.title = 'Organisations';
-   
+
     var unfilteredRows = angular.extend(grid_organisations.rows);
     vm.rows = angular.extend(grid_organisations.rows);
     vm.count = unfilteredRows.length;
-   
+
     vm.options = {
         data: vm.rows,
         enableFiltering: true,
@@ -25,10 +25,10 @@ angular.module('appCg').controller('OrganisationsCtrl', function(grid_organisati
             { name: 'name' },
             { name: 'web_site' },
             { name: 'email_address' },
-            { name: 'referee'},
-            { name: 'auditor'},
-            { name: 'province'},
-            { name: 'organisation_type'}
+            { name: 'referee' },
+            { name: 'auditor' },
+            { name: 'province' },
+            { name: 'organisation_type' }
         ],
         onRegisterApi: function(gridApi) {
             vm.gridApi = gridApi;
@@ -42,11 +42,32 @@ angular.module('appCg').controller('OrganisationsCtrl', function(grid_organisati
 
     vm.openModal = function(id, operation) {
         $uibModal.open({
-            templateUrl: 'partial/programmes/modal/programme-modal.html',
-            controller: 'ProgrammeModalCtrl as vm',
+            templateUrl: 'partial/organisations/modal/organisation-modal.html',
+            controller: 'OrganisationModalCtrl as vm',
             resolve: {
-                programme: function res(gprRestApi) {
-                    return gprRestApi.getRow('programmes', id);
+                organisation: function res(gprRestApi) {
+                    return gprRestApi.getRow('organisations', id);
+                },
+                referees: function res(gprRestApi) {
+                    return gprRestApi.getRows('organisations',false);
+                },
+                auditors: function res(gprRestApi) {
+                    return gprRestApi.getRows('organisations',false);
+                },
+                provinces: function res(gprRestApi) {
+                    return gprRestApi.getRows('provinces',false);
+                },
+                orgTypes: function res(gprRestApi) {
+                    return gprRestApi.getRows('organisation_types',false);
+                },
+                orgStatuses: function res(gprRestApi) {
+                    return gprRestApi.getRows('organisation_statuses',false);
+                },
+                suburbs: function res(gprRestApi) {
+                    return gprRestApi.getRows('suburbs',false);
+                },
+                places: function res() {
+                    return gprRestApi.getRows('places',false);
                 },
                 operation: function res() {
                     return operation;
