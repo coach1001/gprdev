cfc97354bcf677c451be5701306e0212494eb495
Francois Weber <coach1001@gmail.com>
2016-05-09 10:33:32 +0200

    dev environment move to Linux

--- postgrest_run.sh
+++ postgrest_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # postgrest script
-./postgrest postgres://postgres:soulreaver@localhost/gprdev -a postgres -j 'KEYnier#8c40a2hz)s!zvx4*_uz2pv!3xmmfiqq(56_p26h9zi8=-GPR' -p 3002
+./postgrest postgres://postgres:Justice%23%23%40%211996@10.0.0.111/gpr-dev -a postgres -j 'KEYnier#8c40a2hz)s!zvx4*_uz2pv!3xmmfiqq(56_p26h9zi8=-GPR' -p 3002
 
 
